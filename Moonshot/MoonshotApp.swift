469b94679d8f544638091617b4c9e29c204135bf
Leo Chung <leo.chung97@gmail.com>
2023-12-20 12:15:34 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Leo Chung on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
